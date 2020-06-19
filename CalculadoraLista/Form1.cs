6290ad5cb533341f7c748077ad38d3b451982f5f
Matias <matumartinez9@hotmail.com>
2020-06-19 00:09:27 -0300

    Final

---
+++ CalculadoraLista/Form1.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CalculadoraLista
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void buttonC_Click(object sender, EventArgs e)
+        {
+            TB.Clear();
+        }
+
+        private void button0_Click(object sender, EventArgs e)
+        {
+            TB.Text += 0;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            TB.Text += 1;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            TB.Text += 2;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            TB.Text += 3;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            TB.Text += 4;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            TB.Text += 5;
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            TB.Text += 6;
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            TB.Text += 7;
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            TB.Text += 8;
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            TB.Text += 9;
+        }
+
+        private void buttonDot_Click(object sender, EventArgs e)
+        {
+            TB.Text += '.';
+        }
+
+        private void buttonSum_Click(object sender, EventArgs e)
+        {
+            TB.Text += '+';
+        }
+
+
+        private void buttonMin_Click(object sender, EventArgs e)
+        {
+            TB.Text += '-';
+        }
+
+        private void buttonMult_Click(object sender, EventArgs e)
+        {
+            TB.Text += '*';
+        }
+
+        private void buttonDiv_Click(object sender, EventArgs e)
+        {
+            TB.Text += '/';
+        }
+
+        private void buttonEq_Click(object sender, EventArgs e)
+        {
+            string Texto = TB.Text;
+
+            Nodo nuevo = new Nodo();
+
+            nuevo.Fecha = DateTime.Now;
+
+            if (Texto.Contains('+'))
+            {
+                string[] arreglo = Texto.Split('+');
+                nuevo.Numero1 = Convert.ToSingle(arreglo[0]);
+                nuevo.Numero2 = Convert.ToSingle(arreglo[1]);
+                nuevo.Operador = '+';
+
+                nuevo.Resultado = nuevo.Operacion();
+                TB.Text = Convert.ToString(nuevo.Resultado);
+
+                Historial.Items.Add(nuevo.Numero1 + nuevo.Operador.ToString() + nuevo.Numero2 + '=' + nuevo.Resultado + "----->" + nuevo.Fecha);
+                Historial.Refresh();
+
+            } else if(Texto.Contains('-'))
+            { 
+                string[] arreglo = Texto.Split('-');
+                nuevo.Numero1 = Convert.ToSingle(arreglo[0]);
+                nuevo.Numero2 = Convert.ToSingle(arreglo[1]);
+                nuevo.Operador = '-';
+
+                nuevo.Resultado = nuevo.Operacion();
+                TB.Text = Convert.ToString(nuevo.Resultado);
+
+                Historial.Items.Add(nuevo.Numero1 + nuevo.Operador.ToString() + nuevo.Numero2 + '=' + nuevo.Resultado + "----->" + nuevo.Fecha);
+                Historial.Refresh();
+            
+
+            }else if(Texto.Contains('*'))
+            {
+                string[] arreglo = Texto.Split('*');
+                nuevo.Numero1 = Convert.ToSingle(arreglo[0]);
+                nuevo.Numero2 = Convert.ToSingle(arreglo[1]);
+                nuevo.Operador = '*';
+
+                nuevo.Resultado = nuevo.Operacion();
+                TB.Text = Convert.ToString(nuevo.Resultado);
+
+                Historial.Items.Add(nuevo.Numero1 + nuevo.Operador.ToString() + nuevo.Numero2 + '=' + nuevo.Resultado + "----->" + nuevo.Fecha);
+                Historial.Refresh();
+
+            }
+            else if(Texto.Contains('/'))
+            {
+
+                string[] arreglo = Texto.Split('/');
+                nuevo.Numero1 = Convert.ToSingle(arreglo[0]);
+                nuevo.Numero2 = Convert.ToSingle(arreglo[1]);
+                nuevo.Operador = '/';
+
+                if(nuevo.Numero2 != 0)
+                {
+                    nuevo.Resultado = nuevo.Operacion();
+                    TB.Text = Convert.ToString(nuevo.Resultado);
+
+                    Historial.Items.Add(nuevo.Numero1 + nuevo.Operador.ToString() + nuevo.Numero2 + '=' + nuevo.Resultado + "----->" + nuevo.Fecha);
+                    Historial.Refresh();
+                }
+                else
+                {
+                    nuevo.Resultado = -987654321;
+                    TB.Text = "MATH ERROR";
+
+                    Historial.Items.Add(nuevo.Numero1 + nuevo.Operador.ToString() + nuevo.Numero2 + '=' + "MATH ERROR" + "----->" + nuevo.Fecha);
+                    Historial.Refresh();
+                }
+
+            }
+
+        }
+
+        private void Historial_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Historial.Items.Remove(Historial.SelectedItem);
+        }
+    }
+}
---
+++ CalculadoraLista/Nodo.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CalculadoraLista
+{
+    class Nodo
+    {
+        private float numero1;
+        private float numero2;
+        private float resultado;
+        private char operador;
+        private DateTime fecha;
+
+        public float Numero1 { get => numero1; set => numero1 = value; }
+        public float Numero2 { get => numero2; set => numero2 = value; }
+        public float Resultado { get => resultado; set => resultado = value; }
+        public char Operador { get => operador; set => operador = value; }
+        public DateTime Fecha { get => fecha; set => fecha = value; }
+
+
+        public float Operacion()
+        {
+            switch (operador)
+            {
+                case '+':
+                    return numero1 + numero2;
+                case '-':
+                    return numero1 - numero2;
+                case '*':
+                    return numero1 * numero2;
+                case '/':
+                    return numero1 / numero2;
+                default:
+                    return 0;
+
+            }
+        }
+    }
+}
